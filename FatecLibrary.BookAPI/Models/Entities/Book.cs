3a574ec1022f13eab5b8f9b6cc99abdfb9b0f4fc
Neres <joaovneres@outlook.com>
2023-02-10 22:06:50 -0300

    Creating class

--- FatecLibrary.BookAPI/Models/Entities/Book.cs
+++ FatecLibrary.BookAPI/Models/Entities/Book.cs
@@ -2,4 +2,14 @@
 
 public class Book
 {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public decimal Price { get; set; }
+    public int PublicationYear { get; set; }
+    public int Edition { get; set; }
+    public string? ImageURL { get; set; }
+
+
+    public Publishing? Publing { get; set; }
+    public int PublishingId { get; set; }
 }
