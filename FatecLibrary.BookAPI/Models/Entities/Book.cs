8e8828850d802a6a6c3cfb50fc0ffcf224cfa658
Neres <joaovneres@outlook.com>
2023-02-10 21:55:00 -0300

    first commit

---
+++ FatecLibrary.BookAPI/Models/Entities/Book.cs
@@ -0,0 +1,5 @@
+﻿namespace FatecLibrary.BookAPI.Models.Entities;
+
+public class Book
+{
+}
---
+++ FatecLibrary.BookAPI/Models/Entities/Publishing.cs
@@ -0,0 +1,9 @@
+﻿namespace FatecLibrary.BookAPI.Models.Entities;
+
+public class Publishing
+{
+    public int MyProperty { get; set; }
+    public string? Name { get; set; }
+    public string? Acronym { get; set; }
+    public ICollection<Book>? Books { get; set; }
+}
