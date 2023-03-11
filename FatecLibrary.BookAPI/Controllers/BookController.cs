6377d8594f9060e4433abe2a7772d6c90a99bf36
Neres <joaovneres@outlook.com>
2023-03-10 22:12:50 -0300

    Creating Controller

---
+++ FatecLibrary.BookAPI/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using FatecLibrary.BookAPI.DTO.Entities;
+using FatecLibrary.BookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FatecLibrary.BookAPI.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : Controller
+{
+    private readonly IBookService _bookService;
+
+    public BookController(IBookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BookDTO>>> Get()
+    {
+        var bookDTO = await _bookService.GetAll();
+        if (bookDTO is null) return NotFound("Books not found!");
+        return Ok(bookDTO);
+    }
+
+    [HttpGet("{id: int}", Name = "GetBook")]
+    public async Task<ActionResult<BookDTO>> Get(int id)
+    {
+        var bookDTO = await _bookService.GetById(id);
+        if (bookDTO is null) return NotFound("Book not found!");
+        return Ok(bookDTO);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] BookDTO bookDTO)
+    {
+        if (bookDTO is null) return BadRequest("Invalid data!");
+        await _bookService.Create(bookDTO);
+        return new CreatedAtRouteResult("GetBook", new { id = bookDTO.Id }, bookDTO);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put([FromBody] BookDTO bookDTO)
+    {
+        if (bookDTO is null) return BadRequest("Invalid data!");
+        await _bookService.Update(bookDTO);
+        return Ok(bookDTO);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<BookDTO>> Delete(int id)
+    {
+        var bookDTO = await _bookService.GetById(id);
+        if (bookDTO is null) return NotFound("Book not found!");
+        await _bookService.Remove(id);
+        return Ok(bookDTO);
+    }
+}
---
+++ FatecLibrary.BookAPI/Controllers/PublishingController.cs
@@ -0,0 +1,67 @@
+﻿using FatecLibrary.BookAPI.DTO.Entities;
+using FatecLibrary.BookAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FatecLibrary.BookAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PublishingController : Controller
+{
+    private readonly IPublishingService _publishingService;
+
+    public PublishingController(IPublishingService publishingService)
+    {
+        _publishingService = publishingService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PublishingDTO>>> Get()
+    {
+        var publishingDTO = await _publishingService.GetAll();
+        if(publishingDTO is null) return NotFound("Publishers not found!");
+        return Ok(publishingDTO);
+    }
+
+    [HttpGet("books")]
+    public async Task<ActionResult<IEnumerable<PublishingDTO>>> GetPublishersBook()
+    {
+        var publishingDTO = await _publishingService.GetPublishersBooks();
+        if (publishingDTO is null) return NotFound("Publishers not found!");
+        return Ok(publishingDTO);
+    }
+
+    [HttpGet("{id: int}", Name = "GetPublishing")]
+    public async Task<ActionResult<PublishingDTO>> Get(int id)
+    {
+        var publishingDTO = await _publishingService.GetById(id);
+        if (publishingDTO is null) return NotFound("Publishing not found!");
+        return Ok(publishingDTO);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] PublishingDTO publishingDTO)
+    {
+        if(publishingDTO is null) return BadRequest("Invalid data!");
+        await _publishingService.Create(publishingDTO);
+        return new CreatedAtRouteResult("GetPublishing", new {id = publishingDTO.Id}, publishingDTO);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put([FromBody] PublishingDTO publishingDTO)
+    {
+        if (publishingDTO is null) return BadRequest("Invalid data!");
+        await _publishingService.Update(publishingDTO);
+        return Ok(publishingDTO);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<PublishingDTO>> Delete(int id)
+    {
+        var publishingDTO = await _publishingService.GetById(id);
+        if (publishingDTO is null) return NotFound("Publishing not found!");
+        await _publishingService.Remove(id);
+        return Ok(publishingDTO);
+    }
+    
+}
