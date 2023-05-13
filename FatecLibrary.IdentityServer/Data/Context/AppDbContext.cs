44369343eee1ed8cee3f122a7cc06aef5b1db907
Neres <joaovneres@outlook.com>
2023-05-12 22:20:20 -0300

    Create IdentityServer

---
+++ FatecLibrary.IdentityServer/Data/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FatecLibrary.IdentityServer.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FatecLibrary.IdentityServer.Data.Context;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
---
+++ FatecLibrary.IdentityServer/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FatecLibrary.IdentityServer.Data.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    // outra opção para tirar o avio de nulo String.Empty
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+}
