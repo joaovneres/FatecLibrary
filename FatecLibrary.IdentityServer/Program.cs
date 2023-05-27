9fe2957b27423b6d9d2db33e6856b3d56ce40e7a
Neres <joaovneres@outlook.com>
2023-05-26 22:22:31 -0300

    Aula 26/05

--- FatecLibrary.IdentityServer/Program.cs
+++ FatecLibrary.IdentityServer/Program.cs
@@ -1,4 +1,9 @@
+using FatecLibrary.IdentityServer.Configuration;
 using FatecLibrary.IdentityServer.Data.Context;
+using FatecLibrary.IdentityServer.Data.Entities;
+using FatecLibrary.IdentityServer.SeedDataBase.Entities;
+using FatecLibrary.IdentityServer.SeedDataBase.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,32 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     ServerVersion.AutoDetect(mySqlConnection))
     );
 
+// adionando a referência ao Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+// configurando o Identity
+
+var builderIdentityServer = builder.Services.AddIdentityServer(options =>
+{
+    // configurando os eventos do identity em caso de ocorrência
+
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(
+    IdentityConfiguration.IdentityResources)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
+
+// para criar os perfis de usuários eu crio o metódo
+// SeedDataBaseIdentityServer
+builder.Services.AddScoped<IDatabaseInitializer, DatabaseIdentityServerInitializer>();
 
 var app = builder.Build();
 
@@ -28,13 +59,29 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseIdentityServer(); // adicionar aqui
 app.UseAuthorization();
+
+// chamando o metódo SeedDatabaseIdentityServer
+SeedDatabaseIdentityServer(app);
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabaseIdentityServer(IApplicationBuilder app)
+{
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+        // através dessa instância do serviço de IDatabaseInitializer
+        // eu posso chamar os metódos para cirar as regras e os perfis dos usuários
+        var initRoleUsers = serviceScope.ServiceProvider
+            .GetService<IDatabaseInitializer>();
+
+        initRoleUsers.InitializeSeedUsers();
+        initRoleUsers.InitializeSeedRoles();
+    }
+}
