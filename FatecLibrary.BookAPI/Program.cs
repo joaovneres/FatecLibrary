8db7099de39db8828f417bee884f2250d1e892be
Neres <joaovneres@outlook.com>
2023-06-15 17:59:15 -0300

    update

--- FatecLibrary.BookAPI/Program.cs
+++ FatecLibrary.BookAPI/Program.cs
@@ -41,6 +41,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
